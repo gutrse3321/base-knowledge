c8024b59c43f8085caed68b55ce66195be1f1890
gutrse3321 <gutrse3321@live.com>
2019-05-06 23:48:13 +0800

    runtime calculation

--- cpp/sorts/generate_sort/main.cpp
+++ cpp/sorts/generate_sort/main.cpp
@@ -12,6 +12,7 @@ int* generateRandomArray(int n, int rangeL, int rangeR)
 	assert(rangeL <= rangeR);
 
 	int* arr = new int[n];
+  // 随机种子设置，当前时间
 	srand(time(NULL));
 
 	// rand() 随机整数
@@ -42,16 +43,41 @@ void selectionSort(T arr[], int n)
 			if (arr[j] < arr[minIndex])
 				minIndex = j;
 		}
+    // 交换位置值
 		swap(arr[i], arr[minIndex]);
 	}
 }
 
+// 判断是否是正确的排序结果
+template<typename T>
+bool isSorted(T arr[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+    if (arr[i] > arr[i + 1])
+      return false;
+  return true;
+}
+
+// 计算排序运行时间
+template<typename T>
+void test(string sortName, void(*sort)(T[], int), T arr[], int n)
+{
+  clock_t startTime = clock();
+  sort(arr, n);
+  clock_t endTime = clock();
+
+  assert(isSorted(arr, n));
+
+  cout << sortName << ": " << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+  return;
+}
+
 int main()
 {
-	int n = 10;
+	int n = 10000;
 	int* arr = generateRandomArray(n, 2, n);
-	selectionSort(arr, n);
-	printArray(arr, n);
+  test("选择排序", selectionSort, arr, n);
+	// printArray(arr, n);
 
 	// 在helper中new过一个内存，需要将他释放掉
 	delete[] arr;
