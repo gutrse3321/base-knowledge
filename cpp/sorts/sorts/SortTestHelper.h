35c09f4c973fb7fd3b1a689cceca8e8a11f246fd
tomonori <gutrse3321@live.com>
2019-05-06 10:01:07 +0800

    [Code] print array

--- cpp/sorts/sorts/SortTestHelper.h
+++ cpp/sorts/sorts/SortTestHelper.h
@@ -7,16 +7,16 @@ using namespace std;
 
 namespace SortTestHelper
 {
-	// ����n��Ԫ�ص�������飬ÿ��Ԫ�ص������ΧΪrangeL��rangeR
+	// 生成随机数组
 	int* generateRandomArray(int n, int rangeL, int rangeR)
 	{
-		// rangeLҪС��rangeR
+		// rangeL始终小于rangeR
 		assert(rangeL <= rangeR);
 
 		int* arr = new int[n];
 		srand(time(NULL));
 
-		// rand() �����������
+		// rand() 随机整数
 		for (int i = 0; i < n; i++)
 			arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
 		return arr;
