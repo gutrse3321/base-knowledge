34584bf3468e30caae5a59d3a6c333fadd60771e
gutrse3321 <gutrse3321@live.com>
2019-05-17 11:34:11 +0800

    [Change] 封装golang方法

--- golang/sorts/selectionSort.go
+++ golang/sorts/selectionSort.go
@@ -1,31 +1,8 @@
 package main
 
 import (
-  "errors"
-  "time"
-  "math/rand"
-  "fmt"
+  Tools "golang/util"
 )
-
-func init() {
-  rand.Seed(time.Now().UnixNano())
-}
-
-func GenerateRandomArray(n, rangeL, rangeR int) ([]int, error) {
-  var err error
-  if rangeL > rangeR {
-    err = errors.New("rangeL始终小于或等于rangeR")
-    fmt.Println(err)
-    return nil, err
-  }
-
-  arr := make([]int, n)
-  for i := 0; i < n; i++ {
-    temp := rand.Int() % (rangeR - rangeL + 1) + rangeL
-    arr[i] = temp
-  }
-  return arr, nil
-}
 
 func selectionSort(arr []int, n int) {
   for i := 0; i < n; i++ {
@@ -43,9 +20,8 @@ func selectionSort(arr []int, n int) {
 
 func main() {
   var arr []int
-  n := 10
-  arr, _ = GenerateRandomArray(n, 0, n)
-  fmt.Println(arr)
-  selectionSort(arr, n)
-  fmt.Println(arr)
+  n := 100000
+  arr, _ = Tools.GenerateRandomArray(n, 0, n)
+  // 0.0682272s
+  Tools.Test(arr, n, selectionSort, "选择")
 }
